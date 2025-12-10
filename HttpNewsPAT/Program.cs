e4e309eee89fabca5f128f7bf7a6d453cad4a4bb
Kaktys868 <lastochka1230@yandex.ru>
2025-12-10 16:30:17 +0500

    Получение информации с сайта. Булатов

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,23 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+
+            {
+                Console.WriteLine(response.StatusDescription);
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(dataStream))
+                    {
+                        string responseFromServer = reader.ReadToEnd();
+                        Console.WriteLine(responseFromServer);
+
+                    }
+                }
+                
+            }
+            Console.Read();
         }
     }
 }
